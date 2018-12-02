20a0b1981b51409c6e0d30069a63893a89d8cc84
xiaowei <shenzhoudance@gmail.com>
2018-12-02 14:04:40 +0800

    initial commit

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
   chain: 'https://node.cryptape.com',
-  contractAddress: '0x3D91cafAe0460D9cA810bbD03aac89e10A613703',
-  fromAddress: '0x2D785cF68B4aF1D3Cf49f426E09A7bB773BB76c0',
-  privateKey: '0x452afbe580c3680feaf64adc2755d88c7c796d3e622d4f2645557c09a00623f7',
+  contractAddress: '0x7cEB7D878a808B943F78a5089675aDe3EebA7F21',
+  fromAddress: '0xD334C73C59ce914e5Ca1D1faF55A6BE264936D04',
+  privateKey: '0xa835a9a9ec103b704ba990d25f51d039f47cb89061bd5fe55a05197f924832c4',
 }
 
 module.exports = config
